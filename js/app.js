6351da2309ed35a8bb2c4f17f322aebfe9405cf1
Eric Dell'Aringa & Kim Allen <pair+edella2.kimallen@devbootcamp.com>
2016-01-05 12:33:53 -0800

    start ajax request to display users on index page

--- js/app.js
+++ js/app.js
@@ -13,3 +13,28 @@
   //User can go to student's page
   //User can create a badge for that student
   //User can upvote or downvote for a badge
+
+
+// $(document).ready(function(){
+//   indexLoad();
+//   createBadge();
+//   upvoteBadge();
+//   downvoteBadge();
+//   showUser();
+//   addBadgeToUser();
+
+
+// });
+
+
+var users;
+var theTemplateScript = $("#template-index").html();
+$().ajax({
+  url: "http://localhost:3000/users",
+  type: "GET"
+}).then(function(response){
+  users = JSON.parse(response)
+  debugger
+  Handlebars.compile()
+  })
+
---
+++ js/miniQuery.js
@@ -0,0 +1,63 @@
+function miniQuery(selector){
+
+  var selector = document.querySelectorAll(selector)
+
+
+  selector.hide = function(){
+    for (var i = 0; i < selector.length; i++){
+      selector[i].style.visibility = "hidden"
+    }
+  }
+
+  selector.show = function(){
+    for (var i = 0; i < selector.length; i++){
+      selector[i].style.visibility = "visible"
+    }
+  }
+
+  selector.addClass = function(className){
+    for (var i = 0; i < selector.length; i++){
+      selector[i].classList.add(className)
+    }
+  }
+  selector.removeClass = function(className){
+    for (var i = 0; i < selector.length; i++){
+      selector[i].classList.remove(className)
+    }
+  }
+  selector.on = function(eventName, eventHandler) {
+    for (var i = 0; i < selector.length; i++) {
+      selector[i].addEventListener(eventName, eventHandler)
+    }
+  }
+  selector.trigger = function(eventName) {
+    var event = new Event(eventName)
+    for (var i = 0; i < selector.length; i++) {
+      selector[i].dispatchEvent(event);
+    }
+  }
+  selector.ajax = function(options){
+    var url = options["url"];
+    var type = options["type"];
+    var data = options["data"];
+    var promise = new Promise(function(resolve, reject){
+      var query = new XMLHttpRequest();
+      query.open(type, url, true);
+
+      query.onload = function() {
+        if (query.status >= 200 && query.status < 400) {
+          resolve(query.response);
+        } else {
+          reject(query.response);
+        }
+      }
+      query.send(data)
+    })
+    return promise
+  }
+  return selector
+}
+
+var $ = function(selector) {
+  return miniQuery(selector)
+}
